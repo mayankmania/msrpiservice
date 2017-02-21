065a596f627636fea3e8424657ba4675d54a0f60
mayankmania <mayank_p_singh@hotmail.com>
2017-02-21 09:33:14 +0530

    Updated code for client and server

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = new express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+http.listen(process.env.PORT || 9000);
+startUp();
 
 io.on('connection', function (socket) {
     socket.on('operationProcessed', function (operation) {
-        
-        if (operation.description == "post") {
-            io.emit("deviceListFetched", operation.deviceDetails[0]);
+        if (operation.description == "get") {
+            io.emit("deviceListFetched", operation.deviceDetails);
         }
-        else if (operation.description == "get") {
-            io.emit("onDeviceUpdated", operation.deviceDetails);
+        else if (operation.description == "post") {
+            io.emit("onDeviceUpdated", operation.deviceDetails[0]);
         }
     });
 
@@ -23,23 +24,11 @@ io.on('connection', function (socket) {
     });
 });
 
-startUp();
 
-//Listen for all the request
-var server = app.listen(app.get('port'), function () {
-    var port = server.address().port;
-    console.log('Server running on port ' + port);
-});
-
-server.timeout = 300000;
-
-//Register all the startup related stuffs in this function
 function startUp() {
     var options = {
         index: "index.htm"
     };
 
     app.use('/', express.static('public', options));
-    var port = process.env.PORT || 9000;
-    app.set('port', port);
 }
